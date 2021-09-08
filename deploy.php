9d4285f51512f2e34983af7190c683edbb194c1e
Ubuntu <ubuntu@ip-172-31-28-4.ap-southeast-1.compute.internal>
2021-09-08 11:15:58 +0000

    changer 1

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/laravel.php';
 set('application', 'training_deploy');
 
 // Project repository
-set('repository', 'git@domain.com:username/repository.git');
+set('repository', 'git@github.com:duckhang2605/training_deploy.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', false); 
